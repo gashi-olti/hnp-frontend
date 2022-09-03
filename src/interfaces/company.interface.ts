9093fc3d9483cf32bdcbdf926488aa65bc96027f
Oltion Gashi <oltion.gashi@mesh-com.at>
2022-09-03 10:38:23 +0200

    create company and media interfaces

---
+++ src/interfaces/company.interface.ts
@@ -0,0 +1,20 @@
+import { MediaItemType } from './media.interface';
+
+export interface CompanyProfile {
+  uuid?: string;
+  name?: string;
+  number?: string;
+  industry?: string;
+  size?: string;
+  founded?: string;
+  website?: string;
+  description?: string;
+  specialties?: string;
+  // vat_id?: string; - VAT doesn't exist for non-eu countries
+  city?: string;
+  postal_code?: string;
+  country?: string;
+  phone?: string;
+  cover?: MediaItemType | null;
+  media?: MediaItemType[] | null;
+}
