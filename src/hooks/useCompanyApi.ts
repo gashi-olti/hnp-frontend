5a3b7e49974f134d425beaf7224ab8d52fde88fb
Oltion Gashi <oltion.gashi@mesh-com.at>
2022-09-09 23:06:51 +0200

    create company api

---
+++ src/hooks/useCompanyApi.ts
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { useTranslation } from 'next-i18next';
+import useSWR from 'swr';
+
+import Api from '@/lib/api';
+import { useNotification } from '@/providers/NotificationProvider';
+import { CompanyProfile } from '@/interfaces/company.interface';
+import { useAuth } from '@/providers/AuthProvider';
+
+export default function useCompanyApi() {
+  const { isAuthenticated } = useAuth();
+  const {
+    data: company,
+    mutate: mutateCompany,
+    error,
+  } = useSWR<CompanyProfile>(isAuthenticated ? 'company' : null);
+
+  const { t } = useTranslation(['company']);
+
+  const { openSnackbar } = useNotification();
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const updateProfile = async (data: CompanyProfile) => {
+    setIsLoading(true);
+    try {
+      const request = {
+        ...data,
+        media: data.media && data.media.map((media) => ({ ...media })),
+      };
+
+      const body: CompanyProfile = await Api.put('company/profile', request);
+      mutateCompany({ ...company, ...body });
+
+      openSnackbar(t('profile:company profile update success'));
+
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      throw err;
+    }
+  };
+
+  return {
+    company,
+    updateProfile,
+    mutateCompany,
+    error,
+    isFetching: !company && !error,
+    isLoading,
+  };
+}
