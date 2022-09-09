7f231b99bf31406d2c5e3150d7807de098d1523e
Oltion Gashi <oltion.gashi@mesh-com.at>
2022-09-10 01:11:55 +0200

    minor changes

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -10,16 +10,16 @@ type NavLinkProps = {
 
 const NavLink = styled(Link)<NavLinkProps>(({ active }: NavLinkProps) => [
   tw`inline-block relative p-2 md:(px-2 mx-2 my-0)`,
-  tw`after:(absolute content block w-1/5 h-0.5 left-1/2 transform -translate-x-1/2 scale-x-0 bg-white transition-transform)`,
-  !active && tw`hover:after:scale-x-150`,
-  active && tw`after:(scale-x-150)`,
+  tw`after:(absolute content block w-full h-0.5 left-1/2 transform -translate-x-1/2 scale-x-0 bg-white transition-transform)`,
+  !active && tw`hover:after:scale-x-75`,
+  active && tw`after:(scale-x-75)`,
 ]);
 
 export default function Navigation() {
   const router = useRouter();
 
   return (
-    <nav tw="text-white uppercase font-bold text-base md:(ml-4 mt-0)">
+    <nav tw="text-white uppercase font-bold text-base md:(mr-4 mt-0)">
       <NavLink href="/" active={router.pathname === '/'}>
         Home
       </NavLink>
