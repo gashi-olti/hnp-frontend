519e2266245ec0dc698bb4c3274a6037d79686dd
Oltion Gashi <oltion.gashi@mesh-com.at>
2022-09-15 00:05:15 +0200

    update create post url

--- src/components/Header/MobileHeader.tsx
+++ src/components/Header/MobileHeader.tsx
@@ -46,7 +46,7 @@ const getPages = (t: TFunction) => {
     {
       icon: <AddCircleOutlineOutlinedIcon sx={{ color: theme.palette.text.primary }} />,
       label: t('common:create post'),
-      href: '/company/post/create',
+      href: '/company',
     },
     {
       icon: <NotesIcon sx={{ color: theme.palette.text.primary }} />,
