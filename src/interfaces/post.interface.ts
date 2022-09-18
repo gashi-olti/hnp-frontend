22a60ca685c4cfcb970117ee0c20b32566717456
Oltion Gashi <oltion.gashi@mesh-com.at>
2022-09-18 19:38:44 +0200

    remove unused enums

--- src/interfaces/post.interface.ts
+++ src/interfaces/post.interface.ts
@@ -1,29 +1,13 @@
-import { DateTime } from 'luxon';
-
-// export enum JobTypes {
-//   fullTime = 1,
-//   partTime,
-//   internship,
-// }
-
-// export enum JobCategories {
-//   administration = 1,
-//   education,
-//   agronomy,
-//   it,
-// }
-
 export interface PostModel {
-  uuid?: string;
-  title?: string;
-  description?: string;
-  type?: number | null;
-  category?: number | null;
-  location?: string;
-  positions?: number;
+  uuid: string;
+  title: string;
+  description: string;
+  type: number | null;
+  category: number | null;
+  location: string;
+  positions: number;
   experience?: string;
   salary?: string;
-  ends?: string | Date;
-
-  created_at?: string | DateTime;
+  ends: string | Date;
+  created_at?: string | Date;
 }
