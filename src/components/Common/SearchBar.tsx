c79b6497180962f13cd7f280c3978df96cef2517
Oltion Gashi <oltion.gashi@mesh-com.at>
2022-09-18 19:39:22 +0200

    create custom searchbar

--- src/components/Common/SearchBar.tsx
+++ src/components/Common/SearchBar.tsx
@@ -4,12 +4,18 @@ import SearchIcon from '@mui/icons-material/Search';
 import { theme as twinTheme } from 'twin.macro';
 
 export default function SearchBar() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     // setValue(event.target.value);
   };
 
   return (
-    <Card elevation={4} style={{ borderRadius: twinTheme`borderRadius.full` }}>
+    <Card
+      elevation={4}
+      style={{
+        borderRadius: twinTheme`borderRadius.full`,
+      }}
+    >
       <InputBase
         value={null}
         fullWidth
