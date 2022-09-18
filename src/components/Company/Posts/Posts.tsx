645c870c92b103ce3799ed7e3fb079fb2aa697e1
Oltion Gashi <oltion.gashi@mesh-com.at>
2022-09-18 20:31:12 +0200

    wrap in tab panel

--- src/components/Company/Posts/Posts.tsx
+++ src/components/Company/Posts/Posts.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import {
   Box,
   Container,
@@ -7,145 +8,17 @@ import {
   useMediaQuery,
   Button,
   Grid,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  Pagination,
-  TableBody,
-  Tooltip,
-  IconButton,
+  Tabs,
+  Tab,
 } from '@mui/material';
-import { TFunction, useTranslation } from 'next-i18next';
+import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import useSWR from 'swr';
-import { DateTime } from 'luxon';
 
 import SearchBar from '@/components/Common/SearchBar';
-import { Scrollbar } from '@/components/Common/Scrollbar';
-import useSort from '@/hooks/useSort';
-import useFilter from '@/hooks/useFilter';
-import usePagination from '@/hooks/usePagination';
-import { PostModel } from '@/interfaces/post.interface';
-import { PaginatedResult } from '@/interfaces/paginatedResponse.interface';
-import LoadingSpinner from '@/components/LoadingSpinner';
-import { useNotification } from '@/providers/NotificationProvider';
-import usePostApi from '@/hooks/usePostApi';
+import TabPanel from '@/components/Common/TabPanel';
 
-const tableHeadStructure = (t: TFunction) => {
-  const structure = [
-    {
-      name: t('common:name'),
-    },
-    {
-      name: t('common:published'),
-    },
-    {
-      name: t('common:deadline'),
-    },
-    {
-      name: t('common:location'),
-    },
-    {
-      name: t('common:action'),
-    },
-  ];
-
-  return structure;
-};
-
-type RowProps = {
-  row: any;
-  mutatePosts: () => void;
-  t: TFunction;
-};
-
-const PostsRow = ({ row, mutatePosts, t }: RowProps) => {
-  const router = useRouter();
-  const { openConfirmation } = useNotification();
-  const { deletePost } = usePostApi();
-
-  const TooltipTitle = {
-    edit: t('common:edit'),
-    delete: t('common:delete'),
-  };
-
-  const handleDeletePost = async (event: React.MouseEvent<HTMLButtonElement>, uuid: string) => {
-    event.stopPropagation();
-    openConfirmation({
-      title: t('common:are you sure'),
-      content: t('post:post will be deleted'),
-    }).then(async () => {
-      const postResponse = await deletePost(uuid);
-      if (postResponse) mutatePosts();
-    });
-  };
-
-  return (
-    <>
-      <TableRow
-        tw="hover:bg-gray-100 cursor-pointer"
-        onClick={() =>
-          router.push(`/company/posts/${row.uuid}`, `/company/posts/${row.uuid}`, {
-            locale: router.locale,
-          })
-        }
-      >
-        <TableCell key="title" sx={{ maxWidth: 200 }}>
-          <Typography variant="subtitle2">{row.title}</Typography>
-        </TableCell>
-        <TableCell key="created_at" sx={{ maxWidth: 150 }}>
-          <Typography variant="subtitle2">
-            {DateTime.fromISO(row.created_at).toFormat('dd-MM-yyyy')}
-          </Typography>
-        </TableCell>
-        <TableCell key="ends" sx={{ maxWidth: 150 }}>
-          <Typography variant="subtitle2">
-            {DateTime.fromISO(row.ends).toFormat('dd-MM-yyyy')}
-          </Typography>
-        </TableCell>
-        <TableCell key="location" sx={{ maxWidth: 200 }}>
-          <Typography variant="subtitle2">{row.location}</Typography>
-        </TableCell>
-        <TableCell align="right" sx={{ maxWidth: 150 }}>
-          <Grid container columnGap={1} tw="flex justify-end">
-            <Tooltip title={TooltipTitle.edit} arrow>
-              <span>
-                <IconButton
-                  size="small"
-                  edge="end"
-                  aria-label={TooltipTitle.edit}
-                  onClick={() =>
-                    router.push(`/company/posts/${row.uuid}`, `/company/posts/${row.uuid}`, {
-                      locale: router.locale,
-                    })
-                  }
-                >
-                  <EditIcon />
-                </IconButton>
-              </span>
-            </Tooltip>
-            <Tooltip title={TooltipTitle.delete} arrow>
-              <span>
-                <IconButton
-                  size="small"
-                  edge="end"
-                  aria-label={TooltipTitle.edit}
-                  onClick={(post) => handleDeletePost(post, row.uuid)}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </span>
-            </Tooltip>
-          </Grid>
-        </TableCell>
-      </TableRow>
-    </>
-  );
-};
+import ActivePosts from './ActivePosts';
 
 export default function Posts() {
   const { t } = useTranslation(['post', 'common', 'company']);
@@ -153,24 +26,11 @@ export default function Posts() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
-  const sortApi = useSort({ initialSort: { field: 'title', dir: 'asc' } });
-  const filterApi = useFilter();
-  const paginationApi = usePagination();
+  const [value, setValue] = React.useState<string>('activePosts');
 
-  const { page, pageLimit, handleChangePage } = paginationApi;
-
-  const {
-    data: posts,
-    mutate: mutatePosts,
-    error: postsError,
-  } = useSWR<PaginatedResult<PostModel>>(
-    filterApi.searchTermDebounce
-      ? `company/posts?page=${page}&limit=${pageLimit}&order=${sortApi.sort}&dir=${sortApi.sort.dir}&q=${filterApi.searchTermDebounce}`
-      : `company/posts?page=${page}&limit=${pageLimit}&order=${sortApi.sort}&dir=${sortApi.sort.dir}`
-  );
-
-  const noResults = posts?.data.length === 0 && !postsError;
-  const colspan = 7;
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    setValue(newValue);
+  };
 
   return (
     <Container maxWidth="xl">
@@ -215,63 +75,21 @@ export default function Posts() {
 
       <Grid container marginTop={6}>
         <Grid item xs={12}>
-          <Scrollbar>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  {tableHeadStructure(t).map((cell, index) => (
-                    <TableCell
-                      key={index}
-                      align={index === tableHeadStructure(t).length - 1 ? 'right' : 'left'}
-                    >
-                      <Typography variant="h3" color="primary" tw="text-lg">
-                        {cell.name}
-                      </Typography>
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {posts?.data.map((row: any) => {
-                  return <PostsRow key={row.uuid} row={row} mutatePosts={mutatePosts} t={t} />;
-                })}
-                {noResults && (
-                  <TableRow>
-                    <TableCell colSpan={colspan}>
-                      <Typography variant="subtitle2">{t('company:no posts')}</Typography>
-                    </TableCell>
-                  </TableRow>
-                )}
-                {postsError && (
-                  <TableRow>
-                    <TableCell colSpan={colspan}>
-                      <Typography variant="subtitle2">
-                        {t('common:something went wrong')}
-                      </Typography>
-                    </TableCell>
-                  </TableRow>
-                )}
-                {!posts && !postsError && (
-                  <TableRow>
-                    <TableCell colSpan={colspan}>
-                      <LoadingSpinner />
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </Scrollbar>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider', mt: '-2px', mb: 2 }}>
+            <Tabs value={value} onChange={handleChange}>
+              <Tab label={t('common:active posts')} value="activePosts"></Tab>
+              <Tab label={t('common:Expired posts')} value="expiredPosts"></Tab>
+            </Tabs>
+          </Box>
         </Grid>
-        <Grid item xs={12}>
-          <div tw="w-full flex justify-end mt-6">
-            <Pagination
-              variant="outlined"
-              count={posts?.meta.last_page ?? 0}
-              page={page}
-              onChange={handleChangePage}
-            />
-          </div>
-        </Grid>
+
+        <TabPanel value={value} tab="activePosts">
+          <ActivePosts />
+        </TabPanel>
+
+        <TabPanel value={value} tab="expiredPosts">
+          <Typography variant="subtitle2">Coming soon</Typography>
+        </TabPanel>
       </Grid>
     </Container>
   );
